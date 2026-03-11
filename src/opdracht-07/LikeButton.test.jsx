91ebf73ab470772630b36571b87ec9f60234c7b9
lisamast <9027431@student.zadkine.nl>
2026-03-11 14:37:39 +0100

    opdracht 7

--- src/opdracht-07/LikeButton.test.jsx
+++ src/opdracht-07/LikeButton.test.jsx
@@ -14,14 +14,21 @@ describe('LikeButton', () => {
     // TODO: Vind de button (Hint: screen.getByRole('button'))
     // TODO: Klik op de button (Hint: fireEvent.click(button))
     // TODO: Check of "1 likes" op het scherm staat
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    render(<LikeButton initialLikes={0} />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    expect(screen.getByText('1 likes')).toBeInTheDocument(); // Deze test faalt! Vervang met je eigen test
   });
 
   test('verlaagt likes bij tweede klik', () => {
     // TODO: Render LikeButton
     // TODO: Klik 2x op de button
     // TODO: Check of we terug zijn bij "0 likes"
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    render(<LikeButton initialLikes={0} />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    fireEvent.click(button);
+    expect(screen.getByText('0 likes')).toBeInTheDocument(); // Deze test faalt! Vervang met je eigen test
   });
 
   test('toont rood hartje als geliked', () => {
@@ -29,7 +36,9 @@ describe('LikeButton', () => {
     // TODO: Klik op de button
     // TODO: Check of de button tekst "❤️ Like" bevat
     // Hint: screen.getByText(/❤️ Like/)
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    render(<LikeButton initialLikes={0} />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    expect(screen.getByText(/❤️ Like/)).toBeInTheDocument(); // Deze test faalt! Vervang met je eigen test
   });
-
 });
